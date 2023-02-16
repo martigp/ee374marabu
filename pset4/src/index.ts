a8e47d7acb1469c38923e511933e6fa3c47f196b
Gordon Duncan Martinez-Piedra <g@DN0a22706a.SUNet>
2023-02-15 20:48:29 -0800

    renaming node for usssss

--- pset4/src/index.ts
+++ pset4/src/index.ts
@@ -5,7 +5,7 @@ const BIND_PORT = 18018
 const BIND_IP = '0.0.0.0'
 
 logger.info(`Malibu - A Marabu node`)
-logger.info(`Dionysis Zindros <dionyziz@stanford.edu>`)
+logger.info(`Gordon & Mapau`)
 
 async function main() {
   network.init(BIND_PORT, BIND_IP)
