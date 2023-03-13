26ed8a49a3c47ffe94240421297d1d83a7ac284c
mapauhernandez <63224977+mapauhernandez@users.noreply.github.com>
2023-03-12 22:36:13 -0700

    update worker to blake2s

--- pset6/src/worker.c
+++ pset6/src/worker.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,7 +5,7 @@
 #include "blake2.h"
 
 #define TARGET "00000000abc00000000000000000000000000000000000000000000000000000"
-#define TEST_TARGET "00001a00abc00000000000000000000000000000000000000000000000000000"
+#define TEST_TARGET"11001a00abc00000000000000000000000000000000000000000000000000000000"
 #define MAX_SIZE 1024
 
 uint8_t* getNewNonce(uint8_t* nonce) {
@@ -20,7 +19,6 @@ uint8_t* getNewNonce(uint8_t* nonce) {
     }
     return getNewNonce(nonce);
 }
-
 
 void uint82hex(const uint8_t* arr, char* hexStr, size_t hexStrLen) {
     const char* hexChars = "0123456789abcdef";
@@ -42,31 +40,39 @@ void hashBlock(const unsigned char* part1, const unsigned char* nonce, unsigned 
     unsigned int nonce_size = strlen(nonce); 
 
     memcpy(buf, part1, p1_size);
-    strlcat(buf, nonce, p2_size + p1_size +1);
-    strlcat(buf, part2, p2_size + p1_size + nonce_size + 1);
-    int hashed = blake2b(dst, BLAKE2B_OUTBYTES, buf, strlen(buf), NULL, 0);
-
+    buf[p1_size] = '\0';
+    strlcat(buf, nonce, p2_size + p1_size);
+    strlcat(buf, part2, p2_size + p1_size + nonce_size);
+    
+    blake2s_state state;
+    blake2s_init(&state, BLAKE2S_OUTBYTES);
+    blake2s_update(&state, buf, p2_size + p1_size + nonce_size);
+    blake2s_final(&state, dst, BLAKE2S_OUTBYTES);
 }
 
-
-//compile using:  gcc -o worker worker.c ~/BLAKE2/ref/blake2b-ref.c
 int main(int argc, char* argv[]) {
+    char* part1 = argv[1]; 
+    char* part2 = argv[3]; 
+
     char* nonceHex = argv[2]; 
-    uint8_t* nonceArr = malloc(32);
+    uint8_t* nonceArr = malloc(64);
     for (int i = 0; i < 32; i++) {
         sscanf(nonceHex + 2 * i, "%2hhx", &nonceArr[i]);
     }
     unsigned char blockid[BLAKE2B_OUTBYTES]; 
-    hashBlock(argv[1], argv[2], argv[3], blockid); 
-    while(1){ 
-        printf("");
-        if (0 >= strncmp((const char*) blockid, (const char*) TEST_TARGET, 64)) {
-            printf("%s", nonceHex);
-            free(nonceArr);
+    hashBlock(part1, nonceHex, part2, blockid); 
+
+    while(1){
+	printf("%s\n", blockid);  
+      //  uint64_t blockid_num = strtoull(blockid, NULL, 16);
+        if (strcmp(blockid, TARGET) <= 0) {
+            printf("%s\n", nonceHex);
             break;
         }
+
         nonceArr = getNewNonce(nonceArr); 
         uint82hex(nonceArr, nonceHex, 64);
-        hashBlock(argv[0], nonceHex, argv[2], blockid); 
+        hashBlock(part1, nonceHex, part2, blockid); 
     }
 }
+
